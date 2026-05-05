383581ba51627146a96d38780f8bf4142cd39d5c
Levi Costa <leviccosta.dev@gmail.com>
2026-05-05 17:34:54 -0300

    Aula 2

---
+++ aula_02/for_while.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    int n = 1, soma = 0;
+
+    while(n != 0)
+    {
+        cin >> n;
+        soma += n;    
+    }
+
+    cout << soma << endl;
+}
